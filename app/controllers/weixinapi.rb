1917232bc87146b4c32b42ce4e7127c865d6f632
曾令发 <zlfera@gmail.com>
2014-08-17 19:59:04 +0000

    0

--- app/controllers/weixinapi.rb
+++ app/controllers/weixinapi.rb
@@ -28,9 +28,9 @@ Myapp::App.controllers :weixinapi do
   end
   ################################
 
-  get :text, '/wweixin' do
-    params[:echostr]
-  end
+  #get :text, '/weixin' do
+   # params[:echostr]
+  #end
 
   get :text, '/weixin' do
     content_type :xml, charset: 'utf-8'

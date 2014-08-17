4ab5adacd178a33e3992903ad8fda692a79517ab
曾令发 <zlfera@gmail.com>
2014-08-17 21:30:10 +0000

    0

--- app/controllers/weixinapi.rb
+++ app/controllers/weixinapi.rb
@@ -45,7 +45,7 @@ Myapp::App.controllers :weixinapi do
     @message_id = root.css("MsgId").text.to_i
     ###
     # @pic_url = root.css('PicUrl').children.text
-    render 'weixin/text' #("#{@message_type.to_s}")
+    render 'weixinapi/text' #("#{@message_type.to_s}")
 
   end
 

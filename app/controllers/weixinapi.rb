d6135ad7bbb44e2a74126058df6395ac89fea8b8
曾令发 <zlfera@gmail.com>
2014-08-17 19:58:04 +0000

    0

--- app/controllers/weixinapi.rb
+++ app/controllers/weixinapi.rb
@@ -28,11 +28,11 @@ Myapp::App.controllers :weixinapi do
   end
   ################################
 
-  get :text, '/weixin' do
+  get :text, '/wweixin' do
     params[:echostr]
   end
 
-  post :text, '/weixin' do
+  get :text, '/weixin' do
     content_type :xml, charset: 'utf-8'
 
     root = Nokogiri::XML(request.body.read).root

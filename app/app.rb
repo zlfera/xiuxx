df15f554d931961082bf16d37d9c8b9e0e669a60
曾令发 <zlfera@gmail.com>
2014-11-28 23:34:48 +0000

    0

--- app/app.rb
+++ app/app.rb
@@ -52,7 +52,7 @@ module Myapp
     configure :production do
       enable :logging
       GC::Profiler.enable
-      Sinatra::Logentries.token = '122245ee-7cfc-4f89-9eb3-8e05a392c632'
+      #Sinatra::Logentries.token = '122245ee-7cfc-4f89-9eb3-8e05a392c632'
     end
 
     ##
